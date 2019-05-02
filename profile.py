95094efbbc1ac13e07b0363879e928df1a9f0a4d
Jonathan Ellithorpe <jde@cs.stanford.edu>
2019-05-02 16:26:29 +0000

    Updated profile.

--- profile.py
+++ profile.py
@@ -25,13 +25,21 @@ hardware_types = [ ("m510", "m510 (CloudLab Utah, 8-Core Intel Xeon D-1548)"),
                    ("m400", "m400 (CloudLab Utah, 8-Core 64-bit ARMv8)"),
                    ("d430", "d430 (Emulab, 8-Core Intel Xeon E5-2630v3)") ]
 
-pc.defineParameter("image", "Disk Image",
+pc.defineParameter("image_str", "Disk Image URN", 
+        portal.ParameterType.STRING, "", None,
+        "URN of specific disk image to use. If blank, uses option selected in menu above.")
+
+pc.defineParameter("image_opt", "Disk Image",
         portal.ParameterType.IMAGE, images[2], images,
         "Specify the base disk image that all the nodes of the cluster " +\
         "should be booted with.")
 
-pc.defineParameter("hardware_type", "Hardware Type",
+pc.defineParameter("hardware_type_opt", "Hardware Type",
        portal.ParameterType.NODETYPE, hardware_types[0], hardware_types)
+
+pc.defineParameter("hardware_type_str", "Hardware Type", 
+        portal.ParameterType.STRING, "", None,
+        "Specific hardware type to use. If blank, uses option selected in menu above.")
 
 params = pc.bindParameters()
 
@@ -39,8 +47,15 @@ params = pc.bindParameters()
 request = pc.makeRequestRSpec()
 
 node = request.RawPC("master")
-node.hardware_type = params.hardware_type
-node.disk_image = urn.Image(cloudlab.Utah, "emulab-ops:%s" % params.image)
+if (params.harware_type_str != ""):
+    node.hardware_type = params.hardware_type_str
+else:
+    node.hardware_type = params.hardware_type
+
+if (params.image_str != ""):
+    node.disk_image = params.image_str
+else:
+    node.disk_image = urn.Image(cloudlab.Utah, "emulab-ops:%s" % params.image)
 
 # Generate the RSpec
 pc.printRequestRSpec(request)
