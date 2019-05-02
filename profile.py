abfc3e68cef757b76a6c4ac76e407cc9f59c00b6
Jonathan Ellithorpe <jde@cs.stanford.edu>
2019-05-02 16:37:53 +0000

    Added default options for menu selections.

--- profile.py
+++ profile.py
@@ -15,24 +15,25 @@ pc = portal.Context()
 # The possible set of base disk-images that this cluster can be booted with.
 # The second field of every tupule is what is displayed on the cloudlab
 # dashboard.
-images = [ ("UBUNTU14-64-STD", "Ubuntu 14.04"),
-           ("UBUNTU16-64-STD", "Ubuntu 16.04"),
-           ("urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU16-64-STD:45", "Ubuntu 16.04:45") ]
+images = [ ("None", "Select here for preselected images."),
+           ("UBUNTU14-64-STD", "Ubuntu 14.04"),
+           ("UBUNTU16-64-STD", "Ubuntu 16.04") ]
 
 # The possible set of node-types this cluster can be configured with. Currently 
 # only m510 machines are supported.
-hardware_types = [ ("m510", "m510 (CloudLab Utah, 8-Core Intel Xeon D-1548)"),
+hardware_types = [ ("None", "Select here for preselected hardware types."),
+                   ("m510", "m510 (CloudLab Utah, 8-Core Intel Xeon D-1548)"),
                    ("m400", "m400 (CloudLab Utah, 8-Core 64-bit ARMv8)"),
                    ("d430", "d430 (Emulab, 8-Core Intel Xeon E5-2630v3)") ]
+
+pc.defineParameter("image_opt", "Disk Image",
+        portal.ParameterType.IMAGE, images[0], images,
+        "Specify the base disk image that all the nodes of the cluster " +\
+        "should be booted with.")
 
 pc.defineParameter("image_str", "Disk Image URN", 
         portal.ParameterType.STRING, "", None,
         "URN of specific disk image to use. If blank, uses option selected in menu above.")
-
-pc.defineParameter("image_opt", "Disk Image",
-        portal.ParameterType.IMAGE, images[2], images,
-        "Specify the base disk image that all the nodes of the cluster " +\
-        "should be booted with.")
 
 pc.defineParameter("hardware_type_opt", "Hardware Type",
        portal.ParameterType.NODETYPE, hardware_types[0], hardware_types)
